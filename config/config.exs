9d9bdf695eeaf86d2cdf6a7c5acf9076e56dfcab
gondwe <fourty400@gmail.com>
2019-03-27 12:16:08 +0300

    creds

--- config/config.exs
+++ config/config.exs
@@ -31,12 +31,9 @@ config :ueberauth, Ueberauth,
 
 # Configures Ueberauth's Auth0 auth provider
 config :ueberauth, Ueberauth.Strategy.Auth0.OAuth,
-  domain: "dev-qoxi9zcw.auth0.com",
-  # domain: System.get_env("AUTH0_DOMAIN"),
-  client_id: "9H72V1BT9PEcTxoojhr97dcS3O1OnNu6",
-  # client_id: System.get_env("AUTH0_CLIENT_ID"),
-  client_secret: "byLNfrJ6Pi45ehHvIf2RjHmZX-M3lasUL7ROVWT87-X5miCq7AzvfmZFGs25GrkQ"
-  # client_secret: System.get_env("AUTH0_CLIENT_SECRET")
+  domain: System.get_env("AUTH0_DOMAIN"),
+  client_id: System.get_env("AUTH0_CLIENT_ID"),
+  client_secret: System.get_env("AUTH0_CLIENT_SECRET")
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
